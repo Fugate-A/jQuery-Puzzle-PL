33a2d16fd204ca34d86f2f9a404d316f50cd71c0
Andrew Fugate <andrewfugate1121@yahoo.com>
2024-02-25 22:36:40 -0500

    remove comments and updated puzzle 2

--- puzzle2.js
+++ puzzle2.js
@@ -1,9 +1,27 @@
 $(document).ready(function() {
-    // Make puzzle pieces draggable
+    $('.puzzle-piece').each(function() {
+        var containerWidth = $('.puzzle-container').width();
+        var containerHeight = $('.puzzle-container').height();
+        var pieceWidth = $(this).width();
+        var pieceHeight = $(this).height();
+        var randomLeft = Math.floor(Math.random() * (containerWidth - pieceWidth));
+        var randomTop = Math.floor(Math.random() * (containerHeight - pieceHeight));
+        
+        $(this).css({
+            left: randomLeft,
+            top: randomTop
+        });
+    });
+
+    $('.puzzle-container').append('<div class="instruction-text">Please Drag Pieces</div>');
+
     $('.puzzle-piece').draggable({
         containment: '.puzzle-container',
         snap: true,
         snapMode: 'inner',
-        snapTolerance: 10
+        snapTolerance: 10,
+        start: function(event, ui) {
+            $('.instruction-text').hide();
+        }
     });
 });
